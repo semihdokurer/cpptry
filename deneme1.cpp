aa40bcb0a0a481bceda163c93d64774f9cfcad03
semihdokurer <semih@dokurer.net>
2016-01-16 02:10:33 +0200

    değişti

--- deneme1.cpp
+++ deneme1.cpp
@@ -13,9 +13,9 @@ int add(int nValue1, int nValue2)
 
 int main()
 {
-	int Value1, Value2;
-	cin >> Value1;
+	int Value2, Value3;
 	cin >> Value2;
-	cout << add(Value1,Value2) << endl;
+	cin >> Value3;
+	cout << add(Value3,Value2) << endl;
 	return 0;
 }
